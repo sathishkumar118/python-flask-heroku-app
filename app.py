ccd80461fd9bdb196a2efe44ddfc9a124c8e95bf
Sathish Kumar S <ssathishkumar.118@gmail.com>
2021-03-21 19:27:01 +0530

    Update app.py
    Changed the code to get the URL and display the shortened URL

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ app = Flask(__name__, template_folder="templates")           # create an app ins
 @app.route("/", methods =["GET", "POST"])                   # at the end point /
 def test():                    # call method test
     if request.method == "POST": 
-       # getting input with name = fname in HTML form 
-       first_name = request.form.get("fname") 
-       # getting input with name = lname in HTML form  
-       last_name = request.form.get("lname")
-       return "Your name is "+first_name + last_name
-    return render_template("test.html")
+       # getting input url
+       input_url = request.form.get("url")
+       return "Shortened URL is "+input_url.upper()
+    return render_template("url.html")
 if __name__ == "__main__":        # on running python app.py
     app.run(debug=True)                     # run the flask app
