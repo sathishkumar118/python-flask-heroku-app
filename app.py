8028dbee9a61db2e4245ec6f970865fbee6d13f1
Sathish Kumar S <ssathishkumar.118@gmail.com>
2021-03-22 21:51:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # app.py
 from flask import Flask,request,render_template           # import flask, request and render_template
+import os
+import psycopg2
+
 app = Flask(__name__, template_folder="templates")           # create an app instance
 
 @app.route("/", methods =["GET", "POST"])                   # at the end point /
 def test():                    # call method test
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     if request.method == "POST": 
        # getting input url
        input_url = request.form.get("url")
